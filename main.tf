32df3d93b175a60e682ccb00e6676ccf2a1268a3
jpapazian2000 <jpapazian@hashicorp.com>
2021-05-20 11:42:05 +0200

    commit

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-variable "region" {
-  description = "This is the cloud hosting region where your webapp will be deployed."
-}
-
-variable "prefix" {
-  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-}
-
 provider "aws" {
   region = var.region
 }
